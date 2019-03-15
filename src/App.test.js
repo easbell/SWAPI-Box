ca30fd386a487ddd851089c99a27462d3fb9278a
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-15 10:46:39 -0600

    Start testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+// import { fetchPeople } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  describe('fetchVehicles', () => {
+    let mockData;
+  
+    beforeEach(() => {
+      mockData = {
+        name: 'Sand Crawler',
+        model: 'Digger Crawler',
+        class: 'wheeled',
+        passengers: '30'
+      }
+
+      fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve(mockData),
+      }))
+    })
+  
+    it('calls fetchVehicles with the correct url and options', () => {
+      const url = 'www.starwars.com'
+
+      fetchVehicles(url)
+
+      expect(fetch).toHaveBeenCalledWith(url);
+    });
+
+  })
+})
