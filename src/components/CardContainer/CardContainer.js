f0bd89474d4a240f51bf621a89ccc9d07b022cc9
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-17 17:58:14 -0600

    Change CardContainer to functional component.

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import Card from './../Card/Card';
 // import './CardContainer.css';
 
-export class CardContainer extends Component {
-  displayPeople = () => {
+const CardContainer = () =>{
+  const displayCards = () => {
     const { cardsSelected } = this.props
     return cardsSelected.map(card => {
       return <Card key={card.name} cardInfo={{card}}/>
     })
   }
 
-  render() {
-    return (
-      <div className='card-container'>
-        {this.displayPeople()}
-      </div>
-    )
-  }
+  return (
+    <div className='card-container'>
+      {displayCards()}
+    </div>
+  )
 }
 
 export default CardContainer
