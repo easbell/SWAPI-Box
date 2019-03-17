99d0c2df515329a674d45c4b582c8f4dc6326fa5
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-17 17:56:53 -0600

    Add FilterControls snapshot.

--- src/components/FilterControls/FilterControls.test.js
+++ src/components/FilterControls/FilterControls.test.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import FilterControls from './FilterControls';
-import { shallow } from 'enzyme';
-import { mockPeopleResult } from './../../mockData/mockPeopleData'
+import { mount } from 'enzyme';
+
+describe('FilterControls', () => {
+  it('should match snapshot with all data passed in', () => {
+    const mockHandleSort = jest.fn()
+    const wrapper = mount(<FilterControls handleSort={mockHandleSort}/>)
+    
+    expect(wrapper).toMatchSnapshot();
+  })
+})
