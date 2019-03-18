c7e17af6cde1fac46ad25ffed1cb7ec135b1cfb2
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-17 21:15:46 -0600

    Add Scrolling style.

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -3,10 +3,14 @@ import './Splash.css'
 
 const Splash = ({ randomFilm }) => {
   return (
-    <div className='splash'>
-      <p>{randomFilm.openingCrawl}</p>
-      <p>{randomFilm.title}</p>
-      <p>{randomFilm.releaseDate}</p>
+    <div className='fade'>
+      <div className='star-wars'>
+        <div className='crawl'>
+          <p>{randomFilm.openingCrawl}</p>
+          <p>{randomFilm.title}</p>
+          <p>{randomFilm.releaseDate}</p>
+        </div>
+      </div>
     </div>
   )
 }
