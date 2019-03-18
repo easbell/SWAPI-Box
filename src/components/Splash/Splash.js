e50a98fb8ede57c23638ad80b073b1b5912f6f81
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-17 19:57:42 -0600

    Splash Styling.

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Splash.css'
 
 const Splash = ({ randomFilm }) => {
   return (
-    <div>
+    <div className='splash'>
       <p>{randomFilm.openingCrawl}</p>
       <p>{randomFilm.title}</p>
       <p>{randomFilm.releaseDate}</p>
